c18f60f0264b2240367d8fe66a768ff8523c2c25
Vinodbiradar09 <vinodjb07@gmail.com>
2025-08-29 23:38:10 +0530

    models

---
+++ app/model/Letter.ts
@@ -0,0 +1,89 @@
+import mongoose , {model , Schema , Document , models , Model} from "mongoose";
+
+export interface LetterInt extends Document{
+    _id : mongoose.Types.ObjectId,
+    from : mongoose.Types.ObjectId,
+    to : {
+        name : string,
+        email : string,
+        info : string,
+    },
+    fromDate : Date,
+    toDate : Date,
+    totalDays : number,
+    reason : string,
+    subject : string,
+    body : string,
+    status : "Draft" | "Sent",
+    pdfUrl? : string,
+    emailSent : boolean,
+}
+
+const letterSchema = new Schema<LetterInt>(
+    {
+        from : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User",
+            required : [true , "User is required"],
+            index : true,
+        },
+        to : {
+            name : {
+                type : String,
+                required : true,
+            },
+            email : {
+                type : String,
+                required : true,
+            },
+            info : {
+                type : String,
+            }
+        },
+        fromDate : {
+            type : Date,
+            required : [true , "From Date is required"],
+        },
+        toDate : {
+            type : Date,
+            required : [true , "To Date is required"],
+        },
+        totalDays : {
+            type : Number,
+            required : [true , "Total Days is required"],
+        },
+        reason : {
+            type : String,
+            required : [true , "Reason is required"],
+            minLength : [10 , "Atleast 10 chars is required"],
+        },
+        subject : {
+            type : String,
+            required : [true , "Subject is required"],
+            minLength : [10 , "Atleast 10 chars is required"],
+        },
+        body : {
+            type : String,
+            required : [true , "Body is required"],
+            minLength : [25 , "Atleast 25 chars is required"],
+        },
+        status : {
+            type : String,
+            enum : ["Draft" , "Sent"],
+            default : "Draft",
+        },
+        pdfUrl : {
+            type : String,
+        },
+        emailSent : {
+            type : Boolean,
+        }
+    },
+    {
+        timestamps : true,
+    }
+)
+
+const Letter : Model<LetterInt> = models?.Letter as mongoose.Model<LetterInt> || model("Letter" , letterSchema);
+
+export default Letter;
---
+++ app/model/User.ts
@@ -0,0 +1,77 @@
+import mongoose , {model , Schema , Document , models , Model} from "mongoose";
+import bcrypt from "bcrypt";
+
+export interface UserInt extends Document{
+    _id : mongoose.Types.ObjectId,
+    name : string,
+    email : string,
+    usn : string,
+    password : string,
+    department : string,
+    verifyCode : string,
+    verifyCodeExpiry : Date,
+    isVerified : boolean,
+}
+
+const userSchema = new Schema<UserInt>(
+    {
+        name : {
+            type : String,
+            required : [true , "Name is required"],
+            lowercase : true,
+            trim : true,
+            minLength : [4 , "Name must be atleast 4 chars"],
+            maxLength : [20 , "Name can't exceed more than 20 chars"]
+        },
+        email : {
+            type : String,
+            unique : [true , "Email must be unique"],
+            required : [true , "Email address is required"],
+            match : [ /^[^\s@]+@[^\s@]+\.[^\s@]+$/, "please use the valid email address"],
+            index : true,
+        },
+        usn : {
+            type : String,
+            unique : [true , "USN must be unique"],
+            required : [true , "USN is required"],
+            uppercase : true,
+            match : [/^1AY(22|23|24|25|26)[A-Z]{2,3}(0(0[1-9]|[1-9][0-9])|1([0-7][0-9]|80))$/ , "Invalid USN format"],
+            index : true,
+        },
+        password : {
+            type : String,
+            required : [true , "Password is required"],
+            minLength : [6 , "Password must be atleast six chars or digits"],
+        },
+        department : {
+            type : String,
+            required : [true , "Department is required"],
+        },
+        verifyCode : {
+            type : String,
+            required : [true , "Verify Code is required"],
+        },
+        verifyCodeExpiry : {
+            type : Date,
+            required : [true , "Verify Code Expiry is required"],
+        },
+        isVerified : {
+            type : Boolean,
+            default : false,
+        }
+    },
+    {
+        timestamps : true,
+    }
+)
+
+userSchema.pre("save" , async function (next) {
+    if(this.isModified('password')){
+        this.password = await bcrypt.hash(this.password , 10);
+    } 
+    next();
+})
+
+const User : Model<UserInt> = models?.user as mongoose.Model<UserInt> || model("User" , userSchema);
+
+export default User;
