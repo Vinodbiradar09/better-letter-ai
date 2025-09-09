425acab078932295abe02330ad35e2ef95b9add9
Vinodbiradar09 <vinodjb07@gmail.com>
2025-09-09 21:21:53 +0530

    edit

--- app/schemas/UserSchema.ts
+++ app/schemas/UserSchema.ts
@@ -1,15 +1,21 @@
 import { z } from "zod";
 
-export const usnValidation = z.string().
-length(10 , "USN must be 10 characters only").
-regex(/^1AY(22|23|24|25|26)[A-Z]{2,3}((00[1-9]|0[1-9][0-9]|1[0-7][0-9]|180)|40[0-9]|41[0-9]|420)$/
- , "Invalid usn please enter you university seat number correctly").refine((val)=>{
-    const year = parseInt(val.slice(3 , 5));
-    const number = parseInt(val.slice(-3));
-    const validYears = [22 , 23 , 24 , 25 , 26];
-    return validYears.includes(year) && number >= 1 && number <=180
-}, {message : "Usn must belong to batch 2022 to 2026 and number between 001 and 180"});
-
+export const usnValidation = z.string()
+  .length(10, "USN must be 10 characters only")
+  .regex(
+    /^1AY(22|23|24|25|26)[A-Z]{2,3}((00[1-9]|0[1-9][0-9]|1[0-7][0-9]|180)|40[0-9]|41[0-9]|420)$/,
+    "Invalid USN, please enter your university seat number correctly"
+  )
+  .refine(
+    (val) => {
+      const year = parseInt(val.slice(3, 5));
+      const number = parseInt(val.slice(-3));
+      const validYears = [22, 23, 24, 25, 26];
+      return validYears.includes(year) && ((number >= 1 && number <= 180) || (number >= 400 && number <= 420));
+    },
+    { message: "USN must belong to batch 2022 to 2026 and have a valid number (001-180 or 400-420)" }
+  );
+  
 export const verifyCodeValidation = z.object({
     verifyCode : z.string().length(6 , "Verify code must be of six digits"),
 })
